6467115dc7a32cc0a6b0d62acf5d4b128736c7fa
Sergey Ponomarev <stokito@gmail.com>
2023-12-09 18:17:05 +0200

    set_english.py make miwifi.com as a default router IP

--- set_english.py
+++ set_english.py
@@ -9,12 +9,13 @@
 import os
 import ftplib
 
-router_ip_address = input("Router IP address: ")
-# router_ip_address = '192.168.0.21'
+router_ip_address = "miwifi.com"
+# router_ip_address = "192.168.31.1"
+router_ip_address = input("Router IP address: [press enter for using the default '{}']".format(router_ip_address)) or router_ip_address
 
 session = ftplib.FTP(router_ip_address,'root','')
 
-# Uploading a lenguage file will not succeed as the filesystem is ro
+# Uploading a language file will not succeed as the filesystem is ro
 # languageFile = open('extras/language/i18n/base.en.lmo','rb')
 # session.storbinary('STOR /usr/lib/lua/luci/i18n/base.en.lmo', languageFile)
 # languageFile.close()
